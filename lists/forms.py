45a8b674722ed9bd3f0890403c807e2565b3127c
Tímea Tóth <tiemka14@gmail.com>
2017-12-23 15:58:33 +0100

    Implemented duplicate item validation at model layer

--- lists/forms.py
+++ lists/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from lists.models import Item
 
-EMPTY_ITEM_ERROR = "You can't have an empty list item" 
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
 
@@ -19,3 +19,7 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
         }
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
