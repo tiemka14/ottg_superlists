02d05f62f3f6e025f306bf9677a6d7420aaa5a95
Tímea Tóth <tiemka14@gmail.com>
2017-12-27 16:26:04 +0100

    Safer namespacing and onload boilerplate

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,6 @@
+window.Superlists = {};
 
-var initialize = function(){
+window.Superlists.initialize = function(){
   //console.log('initialize called');
   $('input[name="text"]').on('keypress', function(){
     //console.log("in keypress handler")
