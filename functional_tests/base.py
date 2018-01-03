f3bf6069d4a62613967378440954a3397d803e71
Tímea Tóth <tiemka14@gmail.com>
2018-01-03 19:16:18 +0100

    Create Page object for list pages, use in sharing FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -14,6 +14,19 @@ from django.conf import settings
 
 MAX_WAIT = 10
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     """Tests for new visitors."""
     def setUp(self):
@@ -25,18 +38,6 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
-
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except(AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
 
     @wait
     def wait_for_row_in_list_table(self, row_text):
